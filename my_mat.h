1f46d330c2c7434482dea4f0091e0839c6fa9101
edenkabada <93946293+edenkabada@users.noreply.github.com>
2021-12-01 22:25:40 +0200

    Add files via upload

---
+++ my_mat.h
@@ -0,0 +1,7 @@
+void fill_mat();
+void floyd_warshall();
+void path_exists(int i, int j);
+void shortest_path(int i, int j);
+#define MAT_SIZE 10
+ unsigned graph_mat[MAT_SIZE][MAT_SIZE];
+ unsigned int dist[MAT_SIZE][MAT_SIZE];
